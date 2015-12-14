884ac55f20b9868b2e1384638f85faf3da107054
VCSSL Programming Language <rinearn@gmail.com>
2015-12-15 03:00:10 +0900

    GUI.newGraphicsLabel の使用箇所を GUI.newImageLabel に変更、それに伴う変数名の変更など

---
+++ GenerateDocument.bat
@@ -0,0 +1,332 @@
+
+echo: > DocumentIndex.html
+echo ^<html^> >> DocumentIndex.html
+echo ^<head^>^<title^>OpenVCSSLライブラリ APIリファレンス^</title^>^</head^> >> DocumentIndex.html
+echo ^<body^> >> DocumentIndex.html
+echo ^<h1^>OpenVCSSLライブラリ APIリファレンス^</h1^> >> DocumentIndex.html
+echo ^<p^>各ライブラリの詳細仕様を掲載しています。^</p^> >> DocumentIndex.html
+echo ^<hr /^> >> DocumentIndex.html
+
+echo ^<h2^>ライブラリ一覧^</h2^> >> DocumentIndex.html
+
+
+
+echo ^<h3^>^<a href="lib/open/data/DataWrapper.html"^>open.data.DataWrapper ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>任意型の変数値を構造体でラッピングする機能を提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/data/List.html"^>open.data.List ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>リスト型のデータ構造を提供するライブラリです。配列には対応していません。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/data/Queue.html"^>open.data.Queue ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>キュー型のデータ構造を提供するライブラリです。配列には対応していません。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/data/Stack.html"^>open.data.Stack ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>スタック型のデータ構造を提供するライブラリです。配列には対応していません。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/data/WrappedList.html"^>open.data.WrappedList ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>リスト型のデータ構造を提供するライブラリです。配列に対応しています。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/data/WrappedQueue.html"^>open.data.WrappedQueue ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>キュー型のデータ構造を提供するライブラリです。配列に対応しています。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/data/WrappedStack.html"^>open.data.WrappedStack ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>スタック型のデータ構造を提供するライブラリです。配列に対応しています。^</p^> >> DocumentIndex.html
+
+
+
+echo ^<h3^>^<a href="lib/open/system/Int.html"^>open.system.Int ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>int型を補助する機能を提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/system/Float.html"^>open.system.Float ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>float型を補助する機能を提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/system/Complex.html"^>open.system.Complex ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>complex型を補助する機能を提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/system/Bool.html"^>open.system.Bool ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>bool型を補助する機能を提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/system/String.html"^>open.system.String ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>string型を補助する機能を提供するライブラリです。^</p^> >> DocumentIndex.html
+
+
+
+echo ^<h3^>^<a href="lib/open/math/ElementaryFunction.html"^>open.math.ElementaryFunction ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>使用頻度の高い、基本的な数学関数を提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/math/StatisticalFunction.html"^>open.math.StatisticalFunction ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>平均や標準偏差などの統計関数を提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/math/GaussianFunction.html"^>open.math.GaussianFunction ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>ガウス関数を提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/math/FourierTransform.html"^>open.math.FourierTransform ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>離散フーリエ変換処理を提供するライブラリです。^</p^> >> DocumentIndex.html
+
+
+
+echo ^<h3^>^<a href="lib/open/file/TextFile.html"^>open.data.TextFile ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>CSV/TSV形式などのテキストファイル入出力機能を提供します。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/graphics2d/Graphics2DFramework.html"^>open.graphics2d.Graphics2DFramework ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>2DCG（ 2次元グラフィックス ）を手軽に扱うための軽量フレームワークです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/graphics3d/Graphics3DFramework.html"^>open.graphics3d.Graphics3DFramework ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>3DCG（ 2次元グラフィックス ）を手軽に扱うための軽量フレームワークです。^</p^> >> DocumentIndex.html
+
+
+
+echo ^<h3^>^<a href="lib/open/gui/ButtonPanel.html"^>open.gui.ButtonPanel ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>GUI部品として、ボタンが並ぶパネルを提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/ButtonWindow.html"^>open.gui.ButtonWindow ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>GUI部品として、ボタンが並ぶウィンドウを提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/CheckBoxPanel.html"^>open.gui.CheckBoxPanel ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>GUI部品として、チェックボックスが並ぶパネルを提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/CheckBoxWindow.html"^>open.gui.CheckBoxWindow ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>GUI部品として、チェックボックスが並ぶウィンドウを提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/ImageLabelWindow.html"^>open.gui.ImageLabelWindow ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>GUI部品として、画像ラベルが配置されているウィンドウを提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/HorizontalSliderPanel.html"^>open.gui.HorizontalSliderPanel ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>GUI部品として、水平スライダーが並ぶパネルを提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/HorizontalSliderWindow.html"^>open.gui.HorizontalSliderWindow ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>GUI部品として、水平スライダーが並ぶウィンドウを提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/TextFieldPanel.html"^>open.gui.TextFieldPanel ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>GUI部品として、テキストフィールドが並ぶパネルを提供するライブラリです。^</p^> >> DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/TextFieldWindow.html"^>open.gui.TextFieldWindow ライブラリ^</a^>^</h3^> >> DocumentIndex.html
+echo ^<p^>GUI部品として、テキストフィールドが並ぶウィンドウを提供するライブラリです。^</p^> >> DocumentIndex.html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo: ^<hr /^> >> DocumentIndex.html
+
+echo: ^<h2^>全ての関数・定数一覧^</h2^> >> DocumentIndex.html
+
+
+
+
+
+cd ./lib/open/data/
+
+echo ^<h3^>^<a href="lib/open/data/DataWrapper.html"^>open.data.DataWrapper ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>任意型の変数値を構造体でラッピングする機能を提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc DataWrapper.vcssl UTF-8:UTF-8
+call vcssldoc DataWrapper.vcssl UTF-8:UTF-8 -list:lib/open/data/DataWrapper.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/data/List.html"^>open.data.List ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>リスト型のデータ構造を提供するライブラリです。配列には対応していません。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc List.vcssl UTF-8:UTF-8
+call vcssldoc List.vcssl UTF-8:UTF-8 -list:lib/open/data/List.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/data/Queue.html"^>open.data.Queue ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>キュー型のデータ構造を提供するライブラリです。配列には対応していません。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc Queue.vcssl UTF-8:UTF-8
+call vcssldoc Queue.vcssl UTF-8:UTF-8 -list:lib/open/data/Queue.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/data/Stack.html"^>open.data.Stack ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>スタック型のデータ構造を提供するライブラリです。配列には対応していません。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc Stack.vcssl UTF-8:UTF-8
+call vcssldoc Stack.vcssl UTF-8:UTF-8 -list:lib/open/data/Stack.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/data/WrappedList.html"^>open.data.WrappedList ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>リスト型のデータ構造を提供するライブラリです。配列に対応しています。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc WrappedList.vcssl UTF-8:UTF-8
+call vcssldoc WrappedList.vcssl UTF-8:UTF-8 -list:lib/open/data/WrappedList.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/data/WrappedQueue.html"^>open.data.WrappedQueue ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>キュー型のデータ構造を提供するライブラリです。配列に対応しています。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc WrappedQueue.vcssl UTF-8:UTF-8
+call vcssldoc WrappedQueue.vcssl UTF-8:UTF-8 -list:lib/open/data/WrappedQueue.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/data/WrappedStack.html"^>open.data.WrappedStack ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>スタック型のデータ構造を提供するライブラリです。配列に対応しています。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc WrappedStack.vcssl UTF-8:UTF-8
+call vcssldoc WrappedStack.vcssl UTF-8:UTF-8 -list:lib/open/data/WrappedStack.html -body >> ../../../DocumentIndex.html
+
+cd ../../../
+
+
+
+
+
+cd ./lib/open/system/
+
+echo ^<h3^>^<a href="lib/open/system/Int.html"^>open.system.Int ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>int型を補助する機能を提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc Int.vcssl UTF-8:UTF-8
+call vcssldoc Int.vcssl UTF-8:UTF-8 -list:lib/open/system/Int.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/system/Float.html"^>open.system.Float ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>float型を補助する機能を提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc Float.vcssl UTF-8:UTF-8
+call vcssldoc Float.vcssl UTF-8:UTF-8 -list:lib/open/system/Float.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/system/Complex.html"^>open.system.Complex ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>complex型を補助する機能を提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc Complex.vcssl UTF-8:UTF-8
+call vcssldoc Complex.vcssl UTF-8:UTF-8 -list:lib/open/system/Complex.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/system/Bool.html"^>open.system.Bool ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>bool型を補助する機能を提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc Bool.vcssl UTF-8:UTF-8
+call vcssldoc Bool.vcssl UTF-8:UTF-8 -list:lib/open/system/Bool.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/system/String.html"^>open.system.String ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>string型を補助する機能を提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc String.vcssl UTF-8:UTF-8
+call vcssldoc String.vcssl UTF-8:UTF-8 -list:lib/open/system/String.html -body >> ../../../DocumentIndex.html
+
+cd ../../../
+
+
+
+
+
+cd ./lib/open/math/
+
+echo ^<h3^>^<a href="lib/open/math/ElementaryFunction.html"^>open.math.ElementaryFunction ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>使用頻度の高い、基本的な数学関数を提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc ElementaryFunction.vcssl UTF-8:UTF-8
+call vcssldoc ElementaryFunction.vcssl UTF-8:UTF-8 -list:lib/open/math/ElementaryFunction.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/math/StatisticalFunction.html"^>open.math.StatisticalFunction ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>平均や標準偏差などの統計関数を提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc StatisticalFunction.vcssl UTF-8:UTF-8
+call vcssldoc StatisticalFunction.vcssl UTF-8:UTF-8 -list:lib/open/math/StatisticalFunction.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/math/GaussianFunction.html"^>open.math.GaussianFunction ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>ガウス関数を提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc GaussianFunction.vcssl UTF-8:UTF-8
+call vcssldoc GaussianFunction.vcssl UTF-8:UTF-8 -list:lib/open/math/GaussianFunction.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/math/FourierTransform.html"^>open.math.FourierTransform ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>離散フーリエ変換処理を提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc FourierTransform.vcssl UTF-8:UTF-8
+call vcssldoc FourierTransform.vcssl UTF-8:UTF-8 -list:lib/open/math/FourierTransform.html -body >> ../../../DocumentIndex.html
+
+cd ../../../
+
+
+
+
+
+cd ./lib/open/file/
+
+echo ^<h3^>^<a href="lib/open/file/TextFile.html"^>open.data.TextFile ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>CSV/TSV形式などのテキストファイル入出力機能を提供します。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc TextFile.vcssl UTF-8:UTF-8
+call vcssldoc TextFile.vcssl UTF-8:UTF-8 -list:lib/open/file/TextFile.html -body >> ../../../DocumentIndex.html
+
+cd ../../../
+
+
+
+
+
+cd ./lib/open/graphics2d/
+
+echo ^<h3^>^<a href="lib/open/graphics2d/Graphics2DFramework.html"^>open.graphics2d.Graphics2DFramework ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>2DCG（ 2次元グラフィックス ）を手軽に扱うための軽量フレームワークです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc Graphics2DFramework.vcssl UTF-8:UTF-8 -event:event.txt
+call vcssldoc Graphics2DFramework.vcssl UTF-8:UTF-8 -list:lib/open/graphics2d/Graphics2DFramework.html -body -event:event.txt >> ../../../DocumentIndex.html
+
+cd ../../../
+
+
+
+
+
+cd ./lib/open/graphics3d/
+
+echo ^<h3^>^<a href="lib/open/graphics3d/Graphics3DFramework.html"^>open.graphics3d.Graphics3DFramework ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>3DCG（ 2次元グラフィックス ）を手軽に扱うための軽量フレームワークです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc Graphics3DFramework.vcssl UTF-8:UTF-8 -event:event.txt
+call vcssldoc Graphics3DFramework.vcssl UTF-8:UTF-8 -list:lib/open/graphics3d/Graphics3DFramework.html -body -event:event.txt >> ../../../DocumentIndex.html
+
+cd ../../../
+
+
+
+
+
+cd ./lib/open/gui/
+
+echo ^<h3^>^<a href="lib/open/gui/ButtonPanel.html"^>open.gui.ButtonPanel ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>GUI部品として、ボタンが並ぶパネルを提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc ButtonPanel.vcssl UTF-8:UTF-8
+call vcssldoc ButtonPanel.vcssl UTF-8:UTF-8 -list:lib/open/gui/ButtonPanel.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/ButtonWindow.html"^>open.gui.ButtonWindow ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>GUI部品として、ボタンが並ぶウィンドウを提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc ButtonWindow.vcssl UTF-8:UTF-8
+call vcssldoc ButtonWindow.vcssl UTF-8:UTF-8 -list:lib/open/gui/ButtonWindow.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/CheckBoxPanel.html"^>open.gui.CheckBoxPanel ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>GUI部品として、チェックボックスが並ぶパネルを提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc CheckBoxPanel.vcssl UTF-8:UTF-8
+call vcssldoc CheckBoxPanel.vcssl UTF-8:UTF-8 -list:lib/open/gui/CheckBoxPanel.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/CheckBoxWindow.html"^>open.gui.CheckBoxWindow ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>GUI部品として、チェックボックスが並ぶウィンドウを提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc CheckBoxWindow.vcssl UTF-8:UTF-8
+call vcssldoc CheckBoxWindow.vcssl UTF-8:UTF-8 -list:lib/open/gui/CheckBoxWindow.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/ImageLabelWindow.html"^>open.gui.ImageLabelWindow ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>GUI部品として、画像ラベルが配置されているウィンドウを提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc ImageLabelWindow.vcssl UTF-8:UTF-8
+call vcssldoc ImageLabelWindow.vcssl UTF-8:UTF-8 -list:lib/open/gui/ImageLabelWindow.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/HorizontalSliderPanel.html"^>open.gui.HorizontalSliderPanel ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>GUI部品として、水平スライダーが並ぶパネルを提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc HorizontalSliderPanel.vcssl UTF-8:UTF-8
+call vcssldoc HorizontalSliderPanel.vcssl UTF-8:UTF-8 -list:lib/open/gui/HorizontalSliderPanel.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/HorizontalSliderWindow.html"^>open.gui.HorizontalSliderWindow ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>GUI部品として、水平スライダーが並ぶウィンドウを提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc HorizontalSliderWindow.vcssl UTF-8:UTF-8
+call vcssldoc HorizontalSliderWindow.vcssl UTF-8:UTF-8 -list:lib/open/gui/HorizontalSliderWindow.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/TextFieldPanel.html"^>open.gui.TextFieldPanel ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>GUI部品として、テキストフィールドが並ぶパネルを提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc TextFieldPanel.vcssl UTF-8:UTF-8
+call vcssldoc TextFieldPanel.vcssl UTF-8:UTF-8 -list:lib/open/gui/TextFieldPanel.html -body >> ../../../DocumentIndex.html
+
+echo ^<h3^>^<a href="lib/open/gui/TextFieldWindow.html"^>open.gui.TextFieldWindow ライブラリ^</a^>^</h3^> >> ../../../DocumentIndex.html
+echo ^<p^>GUI部品として、テキストフィールドが並ぶウィンドウを提供するライブラリです。^</p^> >> ../../../DocumentIndex.html
+call vcssldoc TextFieldWindow.vcssl UTF-8:UTF-8
+call vcssldoc TextFieldWindow.vcssl UTF-8:UTF-8 -list:lib/open/gui/TextFieldWindow.html -body >> ../../../DocumentIndex.html
+
+cd ../../../
+
+
+
+
+
+echo: ^</body^> >> DocumentIndex.html
+echo: ^</html^> >> DocumentIndex.html
+
